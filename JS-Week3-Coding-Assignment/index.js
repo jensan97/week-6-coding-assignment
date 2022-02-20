156a65c025eb5d6525259189139910763d0cfaad
jensan97 <jennysanchez97@icloud.com>
2022-02-19 22:49:13 -0800

    commit

---
+++ JS-Week3-Coding-Assignment/index.js
@@ -0,0 +1,18 @@
+var ages = [];
+ages.push('3');
+ages.push('9');
+ages.push('23');
+ages.push('64');
+ages.push('2');
+ages.push('8');
+ages.push('28');
+ages.push('93');
+
+console.log(ages[0]);
+console.log(ages[1]);
+console.log(ages[2]);
+console.log(ages[3]);
+console.log(ages[4]);
+console.log(ages[5]);
+console.log(ages[6]);
+console.log(ages[7]);
